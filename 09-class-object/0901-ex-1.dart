e04ce51b1a121f7a029996fefa13bc3a8a72169c
Dhrumil2611 <dhrumilasal0707@gmail.com>
2024-04-04 12:59:24 +0530

    update

---
+++ 09-class-object/0901-ex-1.dart
@@ -0,0 +1,16 @@
+void main (){
+  var student = detailsofstudent();
+  student.display();
+
+}
+
+class detailsofstudent {
+
+  void display(){
+   print("01");
+   print("mahi patel");
+   print("01-01-1011");
+   print(9999999999);
+   print("bhavnagar"); 
+   }
+}
---
+++ 09-class-object/0903-ex-3.dart
@@ -0,0 +1,15 @@
+void main(){
+  var cutmor = customordetails();
+  cutmor.display();
+
+}
+class customordetails {
+ void display(){
+
+print("HDFC");
+print("avenue road");
+print(364011223387);
+print("banglore");
+ }
+  
+}
