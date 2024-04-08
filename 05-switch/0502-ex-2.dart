ad5e4d9115b2446d23bef75f432c4bade3ac6d9a
Dhrumil2611 <dhrumilasal0707@gmail.com>
2024-04-08 11:04:10 +0530

    update

--- 05-switch/0502-ex-2.dart
+++ 05-switch/0502-ex-2.dart
@@ -1,11 +1,12 @@
 void main(){
   int num1 = 12;
   int num2 = 40;
-  switch (num1) {
-    case <40:
+  switch (num1<num2) {
+    case true:
     print("maximum : 40");
        break;
-       
-    default:{print("maximum : 12");}
+     case false:
+    print("maximum : 12");   
+    default:print("both are same");
   }
 }
